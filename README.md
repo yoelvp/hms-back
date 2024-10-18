8187226dc76c8bfd62c1cd60f446109baa6cba4e
yoelvp <yoelvp73@gmail.com>
2024-10-18 10:33:07 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# HMS REST API
+
+REST API for managing hotels, learning Java by failure :)
+
+
+## Installation
+
+```bash
+git clone https://github.com/yoelvp/hms-back.git
+```
+
+```bash
+cd hms-back
+```
+
+```bash
+./gradlew bootStart
+```
+
+## Stack
+
+- Java 17
+- PostgreSQL.
---
+++ src/main/java/com/yoelvp/hms/HotelManagementSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.yoelvp.hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementSoftwareApplication.class, args);
+	}
+
+}
